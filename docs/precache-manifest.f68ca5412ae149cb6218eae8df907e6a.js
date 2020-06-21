422e0e4bea83ff0d501ea1580017824bdb5b4708
Leudy <leudi_rosario@hotmail.com>
2020-06-21 16:51:13 -0400

    addding docs folder to publish gift app

---
+++ docs/precache-manifest.f68ca5412ae149cb6218eae8df907e6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3997abe3c5ac2f23bc5151717c7ad088",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46e9209a663057a9e54c",
+    "url": "/static/css/main.ff15b8c0.chunk.css"
+  },
+  {
+    "revision": "699d45f253e5c07a24f3",
+    "url": "/static/js/2.d563d7a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d563d7a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e9209a663057a9e54c",
+    "url": "/static/js/main.000d1f38.chunk.js"
+  },
+  {
+    "revision": "39e88660e5d512526393",
+    "url": "/static/js/runtime-main.3c337214.js"
+  }
+]);
